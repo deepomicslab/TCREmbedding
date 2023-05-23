6cc78d829fa43df00f36fd42a96055711cbe3344
abearOforest <87287788+abearOforest@users.noreply.github.com>
2023-05-23 16:39:25 +0800

    Delete source/clusTCR/embedding directory

--- source/clusTCR/embedding/test.py
+++
@@ -1,9 +0,0 @@
-from embedding import ClusTCR_embedding
-import pandas as pd
-
-row_data =pd.read_csv(f"../data/combined_dataset.csv", header=0)
-data = row_data['CDR3'].iloc[:10]
-enc = ClusTCR_embedding()
-matrix = enc.encode(data)
-
-print(matrix)
