dd9bf31d007cd0fbc2fc189ab9c5e89879e92593
Miaozhe HUO <35194422+floretli@users.noreply.github.com>
2023-07-10 16:42:46 +0800

    Add files via upload

--- source/TCRanno/TCRanno_embedding.py
+++ source/TCRanno/TCRanno_embedding.py
@@ -3,32 +3,24 @@ import pandas as pd
 
 class EmbeddingTCRanno:
 
-    def __init__(self):
-        self.encoder = None
-        self.cdr3s = None
-
-    def load_model(self, model_path=None):
+    def __init__(self, model_path=None, file_path='./data/sample.csv'):
         """
         Args:
             model_path (str): h5 format autoencoder model. Set model_path=None to use the default model (provided by TCRanno)
+            file_path (str): The path to the CSV file containing TCR sequences.
         """
-
-
+        self.cdr3s = None
+        self.data = pd.read_csv(file_path)
         self.encoder = model_predict.load_encoder(model_path=model_path)
 
-    def read_csv(self, file_path='./data/sample.csv', column_name='aminoAcid'):
-        """Read TCR sequences from a CSV file.
 
-        Args:
-            file_path (str): The path to the CSV file containing TCR sequences.
-            column_name (str): Column name of the provided file recording TCRs. Defaults to 'aminoAcid'.
-        """
-        self.cdr3s = pd.read_csv(file_path)[column_name].tolist()
-
-    def encode(self):
+    def encode(self, column_name='aminoAcid'):
         """
         Get the latent representations of TCRs using the autoencoder model. Each TCR is represented as a 32-dimensional vector.
+        column_name (str): Column name of the provided file recording TCRs. Defaults to 'aminoAcid'.
+
         """
+        self.cdr3s = self.data[column_name].tolist()
         embedding = model_predict.get_norm_latent(self.cdr3s, self.encoder)
         return embedding
 
@@ -36,11 +28,9 @@ if __name__ == '__main__':
     # Load model and data
     file_path = './data/sample.csv'
 
-    embedder = EmbeddingTCRanno()
-    embedder.load_model(model_path = None)  ## set model_path=None to use the default model (provided by TCRanno)
-    embedder.read_csv(file_path='./data/sample.csv', column_name='aminoAcid')
+    TCRanno = EmbeddingTCRanno(model_path=None, file_path='./data/sample.csv') ## set model_path=None to use the default model (provided by TCRanno)
 
     # Get embeddings
-    X = embedder.encode()
+    X = TCRanno.encode(column_name='aminoAcid')
     print(X[:1])
     print(X.shape)
