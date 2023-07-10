c533b06d03eb1b332170d6164ea0918fbe74886a
Miaozhe HUO <35194422+floretli@users.noreply.github.com>
2023-07-10 12:36:57 +0800

    Add files via upload

--- source/TCR2vec/embedding_TCR2vec.py
+++ source/TCR2vec/embedding_TCR2vec.py
@@ -14,33 +14,30 @@ class EmbeddingTCR2vec:
         tcr2vec_model: The TCR2vec model instance. 
     """
 
-    def __init__(self):
-        self.tcr2vec_model = None
+    def __init__(self, model_path, data_path='./data/sample.csv', device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")):
 
-    def load_model(self, model_path, device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")):
-        """Load the pre-trained TCR2vec model.
+        """Load the pre-trained TCR2vec model and dataset.
 
         Args:
             model_path (str): The path to the pre-trained TCR2vec model. 
+            data_path (str): The path to the CSV file containing TCR sequences.
+            
         """
         self.tcr2vec_model = TCR2vec(model_path).to(device)
         self.tcr2vec_model.eval()
+        self.data_path = data_path
+        
 
-    def read_csv(self, file_path='./data/sample.csv', column_name='full_seq'):
-        """Read TCR sequences from a CSV file.
-
-        Args:
-            file_path (str): The path to the CSV file containing TCR sequences.
-            column_name (str): Column name of the provided file recording TCRs. Defaults to 'full_seq'.
-        """
-        self.dset = TCRLabeledDset(file_path, only_tcr=True, use_column=column_name)
-
-    def encode(self, batch_size = 512):
+    def encode(self, batch_size = 512, column_name='CDR3.beta'):
         """Get the embeddings for the loaded TCR sequences.
+        Args:
+            column_name (str): Column name of the provided file recording TCRs.
 
         Returns:
             np.ndarray: The embeddings for the TCR sequences, with shape [n, embed_size].
         """
+        self.dset = TCRLabeledDset(self.data_path, only_tcr=True, use_column=column_name)
+
         loader = DataLoader(self.dset, batch_size=batch_size, collate_fn=self.dset.collate_fn, shuffle=False)
         emb = get_emb(self.tcr2vec_model, loader)
         return emb
@@ -50,10 +47,9 @@ if __name__ == '__main__':
     # Load model and data
     file_path = './data/sample.csv'
 
-    embedder = EmbeddingTCR2vec()
-    embedder.load_model(model_path = './pretrained_models/TCR2vec_120')
-    embedder.read_csv(file_path='./data/sample.csv', column_name='full_seq')
+    ## pretrained TCR2vec models: 1. CDR3vec_120 (pretrained on CDR3 sequences) 2.TCR2vec_120 (pretrained on TCRb FULL sequences)
+    tcr2vec = EmbeddingTCR2vec(model_path = './pretrained_models/CDR3vec_120', data_path='./data/sample.csv' )
 
     # Get embeddings
-    X = embedder.encode()
+    X = tcr2vec.encode(column_name='CDR3.beta')
     print(X.shape)
