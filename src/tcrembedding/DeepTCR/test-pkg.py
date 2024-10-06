aedf41b4e17f3f03604242ea398eb484b96b210f
heli-2 <87287788+heli-2@users.noreply.github.com>
2024-10-06 13:03:16 +0800

    Update test-pkg.py

--- src/tcrembedding/DeepTCR/test-pkg.py
+++ src/tcrembedding/DeepTCR/test-pkg.py
@@ -1,6 +1,6 @@
 from TCRembedding import get_embedding_instance
 
 encoder = get_embedding_instance("EmbeddingDeepTCR")
-encoder.load_model(model_folder_name="Test_Model", Load_Prev_Data=True)
+encoder.load_model(train_data_directory='Data/data', model_folder_name="Test_Model", Load_Prev_Data=True)
 encoder_result = encoder.embed()
 print(encoder_result.shape)
